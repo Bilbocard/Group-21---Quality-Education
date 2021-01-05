9156c78e51fd5d3a4d69f266df6143077d945dcf
Bilbocard <caamserenity@gmail.com>
2021-01-05 22:50:18 +0000

    Removing ignored files
    Remove ignored files

--- quality-education-website/backend/secrets.js
+++
@@ -1,7 +0,0 @@
-// secrets.js
-const secrets = {
-  dbUri:
-    "mongodb://billy:<password>@databasecluster.sl3ly.mongodb.net/<dbname>?retryWrites=true&w=majority",
-};
-
-export const getSecret = (key) => secrets[key];
