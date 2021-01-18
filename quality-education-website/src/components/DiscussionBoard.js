6d843bf77e35eaba73e05adc3139158c036fa127
phoebefrench <phoebe@lancefrench.com>
2021-01-18 12:00:35 +0000

    Discussion Board base file creation

---
+++ quality-education-website/src/components/DiscussionBoard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function DiscussionBoard() {
+    return (
+        <div>
+           <h1> Discussion Board </h1> 
+    
+        </div>
+    )
+} export default DiscussionBoard;
--- quality-education-website/src/components/Home.js
+++ quality-education-website/src/components/Home.js
@@ -1,12 +1,14 @@
 import HeroSection from "./HeroSection.js";
 import React from "react";
 import Cards from "./Cards.js";
+import DiscussionBoard from "./DiscussionBoard.js";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <DiscussionBoard/>
     </>
   );
 }
